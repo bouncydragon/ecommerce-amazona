1fa6c127237e9b2420690eb920bc3f49a4b5e7af
Duane C <duanecarycusi@gmail.com>
2022-11-21 22:05:59 +0800

    added product item, product mock data

---
+++ utils/data.ts
@@ -0,0 +1,30 @@
+const data = {
+  products: [
+    {
+      name: 'Charmander',
+      slug: 'charmander',
+      category: 'Fire Type',
+      image: '/images/pokedex/fire/charmander.png',
+      price: 70,
+      brand: 'Pokemon',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt',
+    },
+    {
+      name: 'Squirtle',
+      slug: 'squirtle',
+      category: 'Water Type',
+      image: '/images/pokedex/water/squirtle.png',
+      price: 70,
+      brand: 'Pokemon',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt',
+    },
+  ],
+};
+
+export default data;
