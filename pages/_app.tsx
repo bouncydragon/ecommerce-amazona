d01deea81680603547470e56a4c71e3702b1901f
Duane C <duanecarycusi@gmail.com>
2022-11-21 23:10:08 +0800

    initial setup of store provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import '../styles/globals.css';
 import '../styles/productItem.css';
 
 import type { AppProps } from 'next/app';
+import { StoreProvider } from '../utils/Store';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
